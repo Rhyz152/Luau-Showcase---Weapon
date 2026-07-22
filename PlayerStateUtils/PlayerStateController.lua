939eb45abf5f87d6fae005ba9d731e55ad3fdb45
Rhyz152 <altturkoise@gmail.com>
2026-07-22 23:51:57 +0100

    Update and rename PlayerStateController.luau to PlayerStateController.lua

--- PlayerStateUtils/PlayerStateController.lua
+++ PlayerStateUtils/PlayerStateController.lua
@@ -1,13 +1,16 @@
+--// Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
 
+--// Connections
 local Connections = ReplicatedStorage:WaitForChild("Connections")
 local PlayerStateConnections = Connections:WaitForChild("PlayerStateConnections")
 local StateEnabledEvent: RemoteEvent = PlayerStateConnections:WaitForChild("StateEnabledEvent")
 
 local module = {}
 
-local PlayerStates: {} = {}
+local PlayerStates: {} = {} -- every player's states
+-- every player gets the same copied version of the States table, makes everything easy
 local States = {
     ["Unarmed"] = true::boolean,
     ["Armed"] = false::boolean,
@@ -16,20 +19,22 @@ local States = {
 }
 
 local function CheckExistingPlayerStates(Player:Player)
-    if not PlayerStates[Player] then
-        return false
+    if PlayerStates[Player] then
+        return true
     end
-    return true
+    
+    return false
 end
 
 local function CheckValidStateName(Player:Player, StateName:string)
-    if PlayerStates[Player][StateName] ~= nil then
+    if PlayerStates[Player][StateName] then
         return true     
     end
     
     return false
 end
 
+-- copying States table to the Player's indivudal state
 function module.InitPlayerStates(Player:Player)
     if CheckExistingPlayerStates(Player) then return end
 
@@ -40,10 +45,11 @@ end
 function module.EnablePlayerState(Player:Player, StateName:string)
     if not CheckExistingPlayerStates(Player) or not CheckValidStateName(Player, StateName) then return end
     if not RunService:IsServer() then
-        Player:Kick("Attempt to enable state from Client? Might be an error, or you should stop exploiting!!!")
+        Player:Kick("Attempt to enable state from Client? Might be an error, or you should stop exploiting!!!") -- check if client is enabling state, not allowed so we kick them out
         return
     end
 
+    -- disable all other states
     for state, value in pairs(PlayerStates[Player]) do
         if state == StateName then continue end
 
@@ -51,7 +57,7 @@ function module.EnablePlayerState(Player:Player, StateName:string)
     end
     
     PlayerStates[Player][StateName] = true
-    StateEnabledEvent:FireClient(Player, StateName)
+    StateEnabledEvent:FireClient(Player, StateName) -- client gets updated on the new state
 end
 
 function module.GetPlayerState(Player:Player, StateName:string)
